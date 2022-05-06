2916ae607658050e229e531dbebc6813848736c0
Mayer Lab <cmayer@neuro.mpg.de>
2022-05-06 15:29:35 +0200

    Update 1_pre_process.sh
    updated out input/output directories

--- TrackerSeq/1_pre_process.sh
+++ TrackerSeq/1_pre_process.sh
@@ -12,40 +12,43 @@
 export PATH=$PATH:/opt/bbmap # to load onto server
 export PATH=$PATH:/Users/mayho/Downloads/samtools-1.9
 
+# write path to input (where your files are) and output (where you want to output files) directories
+datadir=/datastore_share/bioinformatics/datasets/sec/P350_210225_ED_IV_barcode/DualIndex/conc.fastq
+output=/data/mayerlab/mayho/TrackerSeq_demo
 
 ### 1.Trimming and selecting
 
 # Replace in1 and in2 with your R1 and R2 of your retrieved barcode file
-bbduk.sh in1=CloneSeqBarcode200528__S3_R1_001.fastq.gz in2=CloneSeqBarcode200528__S3_R2_001.fastq.gz k=17 literal=GACTCTGGCTCACAAAT ktrim=r out=stdout.fq int=f skipr1 maq=15 |\
-bbduk.sh in=stdin.fq literal=CTGA k=4 restrictleft=4 ktrim=l out1=trim_R1.fastq out2=trim_R2.fastq outm1=dis_R1.fastq outm2=dis_R2.fastq int=t skipr1
+bbduk.sh in1=$datadir/210303_B.conc.R1.fastq.gz in2=$datadir/210303_B.conc.R2.fastq.gz k=17 literal=GACTCTGGCTCACAAAT ktrim=r out=stdout.fq int=f skipr1 maq=15 |\
+bbduk.sh in=stdin.fq literal=CTGA k=4 restrictleft=4 ktrim=l out1=$output/trim_R1.fastq out2=$output/trim_R2.fastq outm1=$output/dis_R1.fastq outm2=$output/dis_R2.fastq int=t skipr1
 
 ### 2. Remove short reads and re-pair
 
 # Remove short reads and re-pair R1 and R2 (for libraries using piggybac)
 
-bbduk.sh in=trim_R2.fastq out=stdout.fq minlength=37 maxlength=37 | \
-repair.sh in1=stdin.fq in2=trim_R1.fastq out1=pair_R1.fastq out2=pair_R2.fastq repair
+bbduk.sh in=$output/trim_R2.fastq out=stdout.fq minlength=37 maxlength=37 | \
+repair.sh in1=stdin.fq in2=$output/trim_R1.fastq out1=$output/pair_R1.fastq out2=$output/pair_R2.fastq repair
 
 ### 3. identify correct cell barcodes
 
 # You may have to adjust the settings for umi_tools whitelist depending on the file for more info see: 
-umi_tools whitelist --stdin $datadir/CloneSeqBarcode200528__S3_R1_001.fastq.gz \ ## change to your R1 file
+umi_tools whitelist --stdin $datadir/210303_B.conc.R1.fastq.gz \
 --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN \
 --knee-method=density \
---expect-cells=3000 \ # change number to the expected cell recovery
+--expect-cells=3000 \
 --plot-prefix=expect_whitelist \
---log2stderr > whitelist.txt
+--log2stderr > $output/MUC28072_whitelist.txt
 
 ### 4. Add the cell barcodes from step 3 to R2 reads 
 
 umi_tools extract --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN \
---stdin pair_R1.fastq \
---stdout pair_R1_extracted.fastq.gz \
---read2-in pair_R2.fastq \
---read2-out=barcode_extracted.fastq \ ## final extracted barcodes
+--stdin $output/pair_R1.fastq \
+--stdout $output/pair_R1_extracted.fastq.gz \
+--read2-in $output/pair_R2.fastq \
+--read2-out=$output/MUC28072_barcode_extracted.fastq \
 --filter-cell-barcode \
 --error-correct-cell \
---whitelist=whitelist.txt;
+--whitelist=$output/MUC28072_whitelist.txt
 
 
 
